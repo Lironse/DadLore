8c18bf543e52d4004727b4f6a04987fe29030151
lironse <lironse2@gmail.com>
2024-06-10 20:09:58 +0300

    error handling in server

--- app/src/routes/publish/+server.ts
+++ app/src/routes/publish/+server.ts
@@ -6,28 +6,32 @@ import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { author, age, date, text } = await request.json();
+	try {
+		if (!author || !date || !text || !age) {
+			return json({ error: 'Missing required fields' }, { status: 400 });
+		}
 
-	if (!author || !date || !text || !age) {
-		return json({ error: 'Missing required fields' }, { status: 400 });
-	}
+		const sanitizedAuthor = author.replace(/[^a-zA-Z0-9_-]/g, '');
+		const sanitizedDate = date.replace(/[^a-zA-Z0-9_-]/g, '');
 
-	const sanitizedAuthor = author.replace(/[^a-zA-Z0-9_-]/g, '');
-	const sanitizedDate = date.replace(/[^a-zA-Z0-9_-]/g, '');
+		const directoryPath = path.join('src', 'routes', '@' + sanitizedAuthor, sanitizedDate);
+		await fs.mkdir(directoryPath, { recursive: true });
 
-	const directoryPath = path.join('src', 'routes', '@' + sanitizedAuthor, sanitizedDate);
-	await fs.mkdir(directoryPath, { recursive: true });
+		const filePath = path.join(directoryPath, '+page.svelte');
 
-	const filePath = path.join(directoryPath, '+page.svelte');
-
-	const fileContent = `
+		const fileContent = `
 <main class='flex flex-col gap-2 m-4'>
 <p>Author: ` + author + ` (` + age + `) </p>
 		<p>date: ` + date + `</p>
 		<p>` + text + `</p>
 </main>`;
 
-	await fs.writeFile(filePath, fileContent);
+		await fs.writeFile(filePath, fileContent);
 
-	const link = path.join('@' + sanitizedAuthor, sanitizedDate)
-	return json({ link: 'dad-lore.vercel.app/' + link });
+		const link = path.join('@' + sanitizedAuthor, sanitizedDate)
+		return json({ link: 'dad-lore.vercel.app/' + link });
+	} catch (e) {
+		console.log('error', e)
+		return json({ link: e });
+	}
 };
